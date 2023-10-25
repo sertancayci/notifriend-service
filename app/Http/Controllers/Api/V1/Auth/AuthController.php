1e6591546c6b187b3de95450ab470c5b32e07fe3
Sertan CAYCI <sertancayci@vatozyazilim.com>
2023-10-25 15:17:32 +0300

    login wip

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Api\BaseController;
 use App\Http\Requests\V1\Auth\RegisterRequest;
+use App\Http\Resources\V1\Auth\LoginResource;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends BaseController
 {
@@ -21,6 +23,32 @@ class AuthController extends BaseController
 
 
         return $this->sendResponse($user, 'Kullanıcı başarıyla kaydoldu.');
+    }
+
+    public function login(Request $request)
+    {
+        
+        $credentials = request(['email', 'password']);
+        if (!Auth::attempt($credentials)) {
+            //if credentials is not true
+            return $this->sendError('Kullanıcı adı veya şifre yanlış', ['error' => 'Unauthorized']);
+            //return error
+        }
+
+        $user = Auth::user();
+
+        if ($user->status != 'ACTIVE') {
+            auth()->user()->tokens()->delete();
+
+            return $this->sendError('Hesabınız pasif durumdadır.');
+        }
+
+        $user->token = $user->createToken('Personal Access Token');
+//        $token = $tokenResult->token;
+        //save token
+
+//        $token->save();
+        return new LoginResource($user);
     }
 
 }
